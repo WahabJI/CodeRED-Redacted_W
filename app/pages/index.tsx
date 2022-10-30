53fe09dede365f32778fa1dfd1a7871b9372d137
WahabJ <wahab.javed@live.com>
2022-10-30 10:30:56 -0500

    Fixing problems

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -18,7 +18,6 @@ import React from 'react'
 //React page component for the homepage
 export default function Home() {  
   const { user, error, isLoading} = useUser();
-  console.log(user);
   return (
     <div className={styles.container}>
       <Head>
@@ -31,18 +30,10 @@ export default function Home() {
             <Image className = "relative inline-block mr-1.5 -mt-0.5" src = "/login.svg" alt="..." width={24} height={24}/>
             <Link href="/api/auth/login" className="no-underline text-white">login</Link>
       </div>
-
       <Logo/>
 
       <SearchBar></SearchBar>
-      <form className = "add">
-        <label htmlFor = "title">TitleTest:</label>
-        <input type = "text" name = "title" required></input>
-        <button>add a new book</button>
-      </form>
       <h2 className="text-marine text-center text-3xl font-semibold mt-52">Personalize Your Suggestions.</h2>
-
-      <h2 id="#activities" className="text-marine text-center text-3xl font-semibold mt-36 mb-4">Personalize Your Suggestions.</h2>
       <Budget></Budget>
       <Activities></Activities>
   
