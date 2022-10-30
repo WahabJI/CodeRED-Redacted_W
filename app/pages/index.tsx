feab2533a7b9edd05bc0cbe0e66995a219078178
Tariq A <tachorzyad@gmail.com>
2022-10-29 19:52:11 -0500

    reinstalled firebase

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -17,9 +17,11 @@ export default function Home() {
       </Head>
       {/* <Header/> */}
       <Logo/>
-      {/* <div className="flex"></div> */}
-      <OutboundSearchBar/>
-      <DestinationSearchBar/>
+      <div className="flex flex-row">
+        <OutboundSearchBar/>
+        <DestinationSearchBar/>
+      </div>
+
       <main className={styles.main}>
         {/* <h1 className={styles.title}>
             Welcome to <a href="https://nextjs.org">Next.js!</a>
