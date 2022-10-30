da603651ea69d733497a1c4e7ac793a671df01d9
gracerabano <gracearabano@gmail.com>
2022-10-30 12:16:27 -0500

    Results Page Finished (?)

--- app/components/resultCards/ResultCards.tsx
+++ app/components/resultCards/ResultCards.tsx
@@ -6,22 +6,30 @@ const resultCards = () => {
       <div className="rounded overflow-hidden shadow-lg">
         {/* <Image className="w-full" src="/mountain.jpg" alt="Mountain"></Image> */}
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Mountain</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, Nonea! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+          <div className="font-bold text-l mb-2">Option 1</div>
+          <div className="font-bold text-xl text-center mb-2">$727</div>
+          <p className="text-gray-700 text-base text-left w-full">
+            <ul className="list-outside">
+                <li><div className="font-bold">Departure Date:</div> 11/07/2022, 10:20AM </li>
+                <li><div className="font-bold">Arrival Date:</div> 11/08/2022, 11:30PM </li>
+                <li><div className="font-bold">Leaving From:</div> Houston, TX (IAH) </li>
+                <li><div className="font-bold">Arriving At:</div> Doha Hamad Intl (DOH) </li>
+                <li><div className ="font-bold">Airline:</div> Multiple Airlines </li>
+                <li><div className="font-bold">Layovers:</div> 2 Layovers (Newark, Istanbul)</li>
+                <li><div className="font-bold">Round Trip:</div>No</li>
+
+            </ul>
           </p>
         </div>
         <div className="px-6 pt-4 pb-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
+            #Backpacker
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
+            #Long Layovers
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
+            #Foodie
           </span>
         </div>
       </div>
@@ -29,22 +37,30 @@ const resultCards = () => {
       <div className="rounded overflow-hidden shadow-lg">
         {/* <Image className="w-full" src="/river.jpg" alt="River"></Image> */}
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">River</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, Nonea! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+          <div className="font-bold text-l mb-2">Option 2</div>
+          <div className="font-bold text-xl text-center mb-2">$1,666</div>
+          <p className="text-gray-700 text-base text-left w-full">
+            <ul className="list-outside">
+                <li><div className="font-bold">Departure Date:</div> 11/07/2022, 10:25AM </li>
+                <li><div className="font-bold">Arrival Date:</div> 11/09/2022 12:05AM </li>
+                <li><div className="font-bold">Leaving From:</div> Houston, TX (IAH) </li>
+                <li><div className="font-bold">Arriving At:</div> Bangkok Suvarnabhumi (BKK) </li>
+                <li><div className ="font-bold">Airline:</div> United Airlines </li>
+                <li><div className="font-bold">Layovers:</div> 1 in Tokyo (NRT)</li>
+                <li><div className="font-bold">Round Trip:</div>Yes, departs from BKK 11/15/2022 back to Houston (IAH)</li>
+
+            </ul>
           </p>
         </div>
         <div className="px-6 pt-4 pb-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
+            #Foodie
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
+            #Outdoorsy
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #summer
+            #Mid-Range Budget
           </span>
         </div>
       </div>
@@ -52,22 +68,30 @@ const resultCards = () => {
       <div className="rounded overflow-hidden shadow-lg">
         {/* <Image className="w-full" src="/forest.jpg" alt="Forest"></Image> */}
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Forest</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, Nonea! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+          <div className="font-bold text-l mb-2">Option 3</div>
+          <div className="font-bold text-xl text-center mb-2">$793</div>
+          <p className="text-gray-700 text-base text-left w-full">
+            <ul className="list-outside">
+                <li><div className="font-bold">Departure Date:</div> 11/07/2022, 7:55AM CST </li>
+                <li><div className="font-bold">Arrival Date:</div> 11/08/2022, 7:50PM CST </li>
+                <li><div className="font-bold">Leaving From:</div> Houston, TX (IAH) </li>
+                <li><div className="font-bold">Arriving At:</div> Taipei City (TPE) </li>
+                <li><div className ="font-bold">Airline:</div> United Airlines </li>
+                <li><div className="font-bold">Layovers:</div> 2 layovers (Las Vegas, San Francisco)</li>
+                <li><div className="font-bold">Round Trip:</div>No</li>
+
+            </ul>
           </p>
         </div>
         <div className="px-6 pt-4 pb-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
+            #Night Life
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
+            #One Way
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #fall
+            #Multiple Layovers
           </span>
         </div>
       </div>
